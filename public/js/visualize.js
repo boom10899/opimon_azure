d49ec959b6679f1898688230d63836b3a2f473c5
Wassapon Watanakeesuntorn <wassapon.w@gmail.com>
2017-04-21 20:08:30 +0700

    update visualize.js (test)

--- public/js/visualize.js
+++ public/js/visualize.js
@@ -528,7 +528,7 @@ function sendNodeLocation() {
     temp["y"] = data["switch"][i]["y"];
     switchNode.push(temp);
 
-    if(i%10 == 0) {
+    if(i%5 == 0) {
       var sendNode = switchNode;
       switchNode = [];
       $.get('/savenode', { switchNode : sendNode })
